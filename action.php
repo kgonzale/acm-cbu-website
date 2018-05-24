3b5f990b4dd7804667cd8982499112ee004669d3
Kevin Gonzalez <kgonzalez@buckman.com>
2018-05-24 11:00:18 -0500

    working over summer

--- action.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            echo $_POST['fName']. " - " .$_POST['lName']. " - " .$_POST['email']. " - " .$_POST['commend'];
-        ?>
-    </body>
-</html> 
